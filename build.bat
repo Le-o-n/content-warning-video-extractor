6fb83db220130e65387e60d1b354c520cf882158
Leon Bass <leonandersonbass@gmail.com>
2024-04-04 13:51:09 +0100

    fixed build to delete the previous build and cleanup all the *.spec files

--- build.bat
+++ build.bat
@@ -1,4 +1,12 @@
 @echo off
+echo Deleting old build folder...
+if exist ".\build\" (
+    rmdir /s /q ".\build"
+)
+echo Building the application...
 call ./install_requirements.bat
-pyinstaller --onefile --distpath ./build main.py
+pyinstaller --onefile --name ContentWarningVideoExtractor --distpath ./build main.py
+echo Cleaning up files
+del /q *.spec
+echo Build and cleanup completed.
 pause
